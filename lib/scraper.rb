8612b98f53aafc1ad7e609fdb70bf6be19ab8c0a
Steve Cordova <cordovas@bc.edu>
2020-08-19 16:31:22 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'open-uri'
 require_relative './course.rb'
 
 class Scraper
-  
+
     def print_courses
     self.make_courses
     Course.all.each do |course|
@@ -15,8 +15,20 @@ class Scraper
       end
     end
   end
-  
+
+  def get_page
+    @doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+
+  end
+
+  def get_courses
+    get_page
+    @doc.css('.post h2')
+
+  end
+
+  def make_courses
+
+
+  end
 end
-
-
-
